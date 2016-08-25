e4085a6ddb80694288a6cf07c055d877e38cbd11
sailendra manne <sailendra manne@DESKTOP-2AVVM12>
2016-08-25 17:43:31 -0400

    d

---
+++ src/bill.java
@@ -0,0 +1,18 @@
+
+public class bill {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+   
+		        electricity Obj = new electricity(); 
+		        Obj.m1(002,"Sailendra", 211); 
+		        double billpay = Obj.billcalculate(); 
+		        Obj.show(); 
+		        System.out.println("Bill to pay : " + billpay); 
+		   } 
+			// TODO Auto-generated method stub
+
+		
+	}
+
---
+++ src/child.java
@@ -0,0 +1,7 @@
+
+class child extends parent {
+void m2()
+{
+	System.out.println("hello");
+}
+}
